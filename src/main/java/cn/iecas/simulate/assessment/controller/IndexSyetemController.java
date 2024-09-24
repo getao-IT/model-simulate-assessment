ba670924e075449cfb27fe942b390a376fcaf02d
getao-IT <778543972@qq.com>
2024-09-24 16:42:55 +0800

    增加swagger访问路径配置

--- src/main/java/cn/iecas/simulate/assessment/controller/IndexSyetemController.java
+++ src/main/java/cn/iecas/simulate/assessment/controller/IndexSyetemController.java
@@ -12,7 +12,6 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;

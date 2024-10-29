19073ea48c9a99707b206aa3950861b960049a56
guo_x <guo_x0315@163.com>
2024-10-29 10:44:02 +0800

    添加：模型评估部分信息鉴权过滤

--- src/main/java/cn/iecas/simulate/assessment/controller/SimulateTaskController.java
+++ src/main/java/cn/iecas/simulate/assessment/controller/SimulateTaskController.java
@@ -40,7 +40,7 @@ public class SimulateTaskController {
 
     @Log("获取仿真任务信息")
     @ApiOperation("获取仿真任务信息")
-    @GetMapping(value = "/getSimulateTaskInfo")
+    @GetMapping(value = "/")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", paramType = "query", value = "任务id", required = false),
             @ApiImplicitParam(name = "pageSize", paramType = "query", value = "分页大小", required = false, defaultValue = "10"),
